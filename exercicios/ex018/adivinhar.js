7d4270a49c6ae4f99f222bbf6a4945ae12497df7
Lucas Oliveira <lucas.eop@gmail.com>
2021-11-20 10:53:52 -0300

    final

---
+++ exercicios/ex018/adivinhar.js
@@ -0,0 +1,16 @@
+var numero = Math.floor(Math.random() * 100 + 1)
+let res = window.document.querySelector('div#final')
+//res.innerHTML += `${numero}`
+function adivinhar(){
+    var palpite =  Number(window.prompt('Qual é o seu palpite?'))
+    if(palpite < numero){
+        res.innerHTML += `<p>Você falou ${palpite}. Meu número é <strong>MAIOR!</strong></p>`
+    }
+    else if(palpite > numero){
+        res.innerHTML += `<p>Você falou ${palpite}. Meu número é <strong>MENOR!</strong></p>`
+    }
+    else{
+        res.innerHTML += `<p><mark>PARABÉNS!</mark> Você acertou! Eu tinha sorteado o valor ${numero}!</p>`
+    }
+
+}
